177b5f771c9bf96841e80322b3e4a1c6d06667cd
P99003673 <78849897+P99003673@users.noreply.github.com>
2021-03-09 14:46:44 +0530

    Add files via upload

---
+++ MiniProject/src/com/ltts/configuration/MyConnection.java
@@ -0,0 +1,17 @@
+package com.ltts.configuration;
+
+	
+	import java.sql.Connection;
+	import java.sql.DriverManager;
+	import java.sql.SQLException;
+
+	public class MyConnection {
+		public static Connection getConnection() throws SQLException
+		{
+			Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/mini","root","root");
+			
+			return c;
+			
+		}
+
+	}
